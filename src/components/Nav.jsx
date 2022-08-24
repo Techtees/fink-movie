bae69e579e2c9e5db358d43142295182a8b50b6b
taiwoj121 <taiwoj121@gmail.com>
2022-08-24 13:45:51 +0100

    increased width of Nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ const data = ['Movies','Tv Shows']
 
 const Navbar = (props)=> {
     return(
-        <div className='w-11/12 mx-auto max-w-5xl flex justify-between items-start py-5'>
+        <div className='w-11/12 mx-auto max-w-6xl flex justify-between items-start py-5'>
             <img src={logo} />
             <ul>
                 {data.map(list=> <li className='inline mr-5  hover:border-b-2 border-green' key={list} style={{cursor:'pointer'}}>{list}</li>)}
